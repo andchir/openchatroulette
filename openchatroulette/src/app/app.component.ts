30e964daa85b2483283a56d4c469e8c1af61392b
andrew <andchir@gmail.com>
2022-09-01 01:49:19 +0300

    layout update

--- openchatroulette/src/app/app.component.ts
+++ openchatroulette/src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
+
+declare const window: Window;
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'openchatroulette';
+export class AppComponent implements OnInit {
+
+    videoHeight = 400;
+
+    constructor() {
+    }
+
+    @HostListener('window:resize', ['$event.target'])
+    onResize(window: Window): void {
+        const footerHeight = 250;
+        const windowWidth = window.innerWidth;
+        const windowHeight = window.innerHeight;
+        if (windowWidth < 992) {
+            this.videoHeight = 300;
+        } else {
+            this.videoHeight = Math.floor(windowHeight - footerHeight);
+        }
+    }
+
+    ngOnInit(): void {
+        this.onResize(window);
+    }
 }
