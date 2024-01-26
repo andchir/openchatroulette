ec5de99e9e90607db3b77985e769f8f3ed80c0f2
Andrei Chirko <andchir@gmail.com>
2024-01-26 14:00:36 +0300

    environment fileupd

--- openchatroulette/src/environments/environment.prod.ts
+++ openchatroulette/src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
     production: true,
     port: 9000,
     peerServerPath: '/openchatroulette',
-    secure: false,
+    secure: true,
     stun_urls: 'stun:stun.l.google.com:19302',
     turn_urls: ['turn:eu-0.turn.peerjs.com:3478', 'turn:us-0.turn.peerjs.com:3478'],
     turn_username: 'peerjsp',
