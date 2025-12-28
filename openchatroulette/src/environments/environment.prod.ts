74300702c1696227a5a9fd29ddcbdc24fa2d34c6
andrew <andchir@gmail.com>
2025-12-29 00:23:11 +0300

    upd

--- openchatroulette/src/environments/environment.prod.ts
+++ openchatroulette/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
     peerServerHost: '/',
-    peerServerPort: 9000,
+    peerServerPort: 443,
     peerServerPath: '/openchatroulette',
     secure: true,
     stun_urls: 'stun:stun.l.google.com:19302',
